5d0663e05c045e62b7f94217a971faf416ff557a
DarkmaterGM <86495748+DarkmaterGM@users.noreply.github.com>
2026-06-08 09:30:39 +0200

    Delete proyecto directory

--- proyecto/src/DTO/ProductoNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package DTO;
-
-public class ProductoNoEncontradoException extends Exception{
-	public ProductoNoEncontradoException(String id) {
-		super("Producto con id "+ id +" no encontrado");
-	}
-}
